328d4c08da25390696742d8c63a2837f26dcb682
Nassim Tinkicht <nassim.tinkicht@gmail.com>
2026-09-11 01:52:08 +0000

    fix(outbox): reject acronym-leading sensitive keys

--- src/modules/notification-outbox/index.ts
+++ src/modules/notification-outbox/index.ts
@@ -52,6 +52,7 @@ const sensitivePayloadKey =
 
 function normalizePayloadKey(key: string): string {
   return key
+    .replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2')
     .replace(/([a-z0-9])([A-Z])/g, '$1_$2')
     .replace(/[^a-z0-9]+/gi, '_')
     .toLowerCase();
