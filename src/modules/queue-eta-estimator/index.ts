1ac35675a193179927c4a72d878ded83e3dda619
Nassim Tinkicht <nassim.tinkicht@gmail.com>
2026-09-18 15:14:43 +0400

    feat(queue-eta): export summary formatter

--- src/modules/queue-eta-estimator/index.ts
+++ src/modules/queue-eta-estimator/index.ts
@@ -100,4 +100,8 @@ export function computeQueueEtaRange({
   };
 }
 
+export {
+  formatWaitRangeSummary,
+  type FormattedWaitRangeSummary,
+} from './summary-format';
 export { summarizeWaitRange, type WaitRangeSummary } from './summary';
